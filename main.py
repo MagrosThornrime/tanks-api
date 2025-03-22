c34a0f54ea6ad430403be099a98fdef52cdbc58f
MagrosThornrime <magros.thornrime@gmail.com>
2025-03-22 19:07:52 +0100

    Finish grinding path finder

--- main.py
+++ main.py
@@ -4,11 +4,39 @@ import json
 from collections import namedtuple
 from dotenv import load_dotenv
 import os
+from dataclasses import dataclass
 
 
 load_dotenv()
 WOT_KEY = os.getenv("WOT_KEY")
-Resources = namedtuple("Resources", ["credits", "exp"])
+
+@dataclass
+class Cost:
+    credits: int = 0
+    xp: int = 0
+
+    def __add__(self, other):
+        return Cost(self.credits + other.credits, self.xp + other.xp)
+
+    def __eq__(self, other):
+        return self.xp == other.xp and self.credits == other.credits
+
+    def __gt__(self, other):
+        if self.xp > other.xp:
+            return True
+        return self.xp == other.xp and self.credits > other.credits
+
+    def __lt__(self, other):
+        if self.xp < other.xp:
+            return True
+        return self.xp == other.xp and self.credits < other.credits
+
+    def __ge__(self, other):
+        return self.__gt__(other.xp) or self.__eq__(other.xp)
+
+    def __le__(self, other):
+        return self.__lt__(other.xp) or self.__eq__(other.xp)
+
 
 def find_tank(tanks: dict, name: str) -> dict:
     for params in tanks.values():
@@ -16,31 +44,32 @@ def find_tank(tanks: dict, name: str) -> dict:
             return params
     return None
 
-def traverse_modules(tanks_found: dict, visited: dict, modules: dict, module_id: str, xp: int = 0, indent: int = 0):
-    module = modules[module_id]
 
+def traverse_modules(tanks_found: dict, visited: dict, modules: dict, module_id: str, xp: int = 0):
     if visited[module_id]:
         return
     visited[module_id] = True
-
-    field = indent * 4 * " "
-    print(field+f"module: {module_id}")
-    print(field+f"xp: {xp}")
+    module = modules[module_id]
     if module["next_modules"]:
         for next_id in module["next_modules"]:
             next_id = str(next_id)
             next_xp = modules[next_id]["price_xp"]
-            traverse_modules(tanks_found, visited, modules, next_id, xp + next_xp, indent+1)
-    
+            traverse_modules(tanks_found, visited, modules, next_id, xp + next_xp)
     if module["next_tanks"]:
-        for tank in module["next_tanks"]:
-            print(field+f"tank: {tank}")
-            tank = str(tank)
-            tanks_found[tank] = xp
-    print()
+        for tank_id in module["next_tanks"]:
+            tank_id = str(tank_id)
+            tanks_found[tank_id] = xp
 
 
-def resources_next_tanks(tanks: dict, current_tank_name: str) -> dict[str, Resources]:
+def tank_research_price(tanks: dict, current_tank_id: str, next_tank_id: str) -> Cost:
+    next_tank = tanks[next_tank_id]
+    prices_xp = next_tank["prices_xp"]
+    xp = prices_xp[current_tank_id]
+    credits = next_tank["price_credit"]
+    return Cost(credits, xp)
+
+
+def get_next_tank_costs(tanks: dict, current_tank_name: str) -> dict[str, Cost]:
     current_tank = find_tank(tanks, current_tank_name)
     if current_tank is None:
         raise ValueError("Tank not found")
@@ -49,24 +78,81 @@ def resources_next_tanks(tanks: dict, current_tank_name: str) -> dict[str, Resou
     tanks_found = {}
     for module_id in modules:
         traverse_modules(tanks_found, visited, modules, module_id)
-    for tank in tanks_found:
-        # new_tank_xp = tanks[tank]["prices_xp"][current_tank]
-        # tanks_found[tank] += tanks[tank][current_tank]
-    return tanks_found
+    current_tank_id = str(current_tank["tank_id"])
+    tanks_found_costs = {}
+    for tank_id, xp in tanks_found.items():
+        research_price = tank_research_price(tanks, current_tank_id, tank_id)
+        tanks_found_costs[tank_id] = Cost(research_price.credits, research_price.xp + xp)
+    return tanks_found_costs
+
+
+def traverse_tanks(tanks: dict, current_tank_id: str, goal_tank_id: str, 
+                    current_path: tuple[str], current_cost: Cost, indent=0) -> Cost:
+    if current_tank_id == goal_tank_id:
+        return current_cost, current_path
+
+    goal_tank = tanks[goal_tank_id]
+    current_tank = tanks[current_tank_id]
+    if int(current_tank["tier"]) >= int(goal_tank["tier"]):
+        return None, None
+
+    next_tanks = get_next_tank_costs(tanks, current_tank["name"])
+    field = indent * 4 * " "
+    print(f"{field}current tank: {tanks[current_tank_id]["name"]}")
+
+    min_cost, min_path = None, None
+    for next_tank_id, research_cost in next_tanks.items():
+        print(f"{field}next tank: {tanks[next_tank_id]["name"]}")
+        print(f"{field}{current_cost}")
+        print(f"{field}{research_cost}")
+        print(f"{field}{current_cost + research_cost}")
+        next_cost, next_path = traverse_tanks(tanks, next_tank_id, goal_tank_id,
+                                              current_path + (next_tank_id,),
+                                              current_cost + research_cost, indent+1)
+        if next_cost is None:
+            print(f"{field}wrong path")
+            continue
+        if min_cost is None or next_cost < min_cost:
+            min_cost = next_cost
+            min_path = next_path
+            print(f"{field}good path")
+    return min_cost, min_path
+
+
+def get_grind_path(tanks: dict, current_tank_name: str, goal_tank_name: str) -> Cost:
+    current_tank = find_tank(tanks, current_tank_name)
+    if current_tank is None:
+        raise ValueError("Current tank not found")
+    goal_tank = find_tank(tanks, goal_tank_name)
+    if goal_tank is None:
+        raise ValueError("Goal tank not found")
+    
+    current_tank_id = str(current_tank["tank_id"])
+    goal_tank_id = str(goal_tank["tank_id"])
+    cost, path = traverse_tanks(tanks, current_tank_id, goal_tank_id, (), Cost())
+    if cost is None:
+        raise ValueError("Path not found")
+    path = (current_tank_id,) + path
+    path_names = [tanks[tank_id]["name"] for tank_id in path]
+    return {
+        "path": path_names,
+        "xp": cost.xp,
+        "credits": cost.credits
+    }
 
 
 async def main():
     async with aiohttp.ClientSession() as session:
-        async with session.get(f"https://api.worldoftanks.eu/wot/encyclopedia/vehicles/?application_id={WOT_KEY}&nation=ussr&tier=9&type=mediumTank") as resp:
+        async with session.get(f"https://api.worldoftanks.eu/wot/encyclopedia/vehicles/?application_id={WOT_KEY}&nation=ussr") as resp:
             print(resp.status)
             text = await resp.text()
             russian_tanks = json.loads(text)["data"]
             for tank_id in russian_tanks.keys():
                 russian_tanks[tank_id]["provisions"] = []
             
-            names = [tank["name"] for tank in russian_tanks.values()]
-            print(names)
-            print(resources_next_tanks(russian_tanks, "T-54"))
+
+            print(get_grind_path(russian_tanks, "BT-5", "T-10"))
+
 
 
 asyncio.run(main())
